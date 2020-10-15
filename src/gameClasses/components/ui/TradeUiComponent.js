7214bbfde7ec437e8970ae8e00449094f341e5c3
parth vakharia <pvakharia007@gmail.com>
2020-10-15 20:10:35 +0530

    fix: trading

--- src/gameClasses/components/ui/TradeUiComponent.js
+++ src/gameClasses/components/ui/TradeUiComponent.js
@@ -5,35 +5,35 @@ var TradeUiComponent = IgeEntity.extend({
     init: function (entity, options) {
         var self = this;
 
-        $("#accept-trade-request-button").on("click", function () {
+        $('#accept-trade-request-button').on('click', function () {
             var requestedBy = $('#requested-by').text();
             var acceptedBy = ige.client.myPlayer.id();
             ige.network.send('trade', { type: 'start', requestedBy: requestedBy, acceptedBy: acceptedBy });
-            $("#trade-request-div").hide();
+            $('#trade-request-div').hide();
             ige.tradeUi.startTrading(ige.client.myPlayer, ige.$(requestedBy));
-        })
+        });
 
-        $("#accept-trade-button").on("click", function () {
+        $('#accept-trade-button').on('click', function () {
             ige.network.send('trade', {
                 type: 'accept',
                 acceptedBy: ige.client.myPlayer.id(),
-                acceptedFor: ige.client.myPlayer.tradingWith
+                acceptedFor: ige.client.myPlayer.tradingWith,
             });
-        })
+        });
 
-        $(".cancel-trade-request-button").on("click", function () {
+        $('.cancel-trade-request-button').on('click', function () {
             // ige.network.send('trade', { type: 'cancel' });
             ige.tradeUi.closeTradeRequest();
-        })
+        });
 
-        $(".cancel-trade-button").on("click", function () {
+        $('.cancel-trade-button').on('click', function () {
             // ige.network.send('trade', { type: 'cancel' });
             ige.tradeUi.closeTrading();
         });
     },
 
     initiateTradeRequest: function (player) {
-        var message = player._stats.name + " wants to trade with you. Trade?";
+        var message = player._stats.name + ' wants to trade with you. Trade?';
         $('#requested-by').text(player.id());
         $('#trade-request-message').text(message);
         $('#trade-request-div').show();
@@ -42,8 +42,7 @@ var TradeUiComponent = IgeEntity.extend({
     startTrading: function (playerA, playerB) {
         if (playerA !== ige.client.myPlayer) {
             $('#trader-name').text(playerA._stats.name);
-        }
-        else if (playerB !== ige.client.myPlayer) {
+        } else if (playerB !== ige.client.myPlayer) {
             $('#trader-name').text(playerB._stats.name);
         }
         playerA.tradingWith = playerB.id();
@@ -54,7 +53,7 @@ var TradeUiComponent = IgeEntity.extend({
     },
     sendOfferingItems: function () {
         var selectedUnit = ige.client.myPlayer.getSelectedUnit();
-        var totalInventorySlot = selectedUnit._stats.inventorySize;
+        var totalInventorySlot = selectedUnit.inventory.getTotalInventorySize();
         var tradeItems = [];
         var id = 0;
         for (var i = totalInventorySlot; i < totalInventorySlot + 5; i++) {
@@ -64,7 +63,7 @@ var TradeUiComponent = IgeEntity.extend({
             type: 'offer',
             from: ige.client.myPlayer.id(),
             to: ige.client.myPlayer.tradingWith,
-            tradeItems: tradeItems
+            tradeItems: tradeItems,
         });
     },
     receiveOfferingItems: function (tradeItems) {
@@ -76,12 +75,10 @@ var TradeUiComponent = IgeEntity.extend({
             if (itemId && item && item._category === 'item') {
                 var itemDiv = ige.itemUi.getItemDiv(item, {
                     isDraggable: false,
-                    popover: "top",
-                    isTrading: true
+                    popover: 'top',
+                    isTrading: true,
                 });
-                $('#offer-' + index).html(
-                    itemDiv
-                );
+                $('#offer-' + index).html(itemDiv);
             }
         }
     },
@@ -101,5 +98,6 @@ var TradeUiComponent = IgeEntity.extend({
     },
 });
 
-
-if (typeof (module) !== 'undefined' && typeof (module.exports) !== 'undefined') { module.exports = TradeUiComponent; }
+if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = TradeUiComponent;
+}
