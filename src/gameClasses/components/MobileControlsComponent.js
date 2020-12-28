8bcee586370ac9e727c1d879a91e4a86c25e08a3
acheo <support@unitedcommand.com>
2020-12-28 03:33:57 +0000

    Remove dead and input event handling code.

--- src/gameClasses/components/MobileControlsComponent.js
+++ src/gameClasses/components/MobileControlsComponent.js
@@ -20,8 +20,6 @@ var MobileControlsComponent = IgeEntity.extend({
 
         this.controls = [];
 
-        this.touchesDown = []; // track downness of touches to mitigate move after up issue
-
         var canvas = document.getElementById('igeFrontBuffer');
         this.canvas = {
             height: (canvas.style.height && parseInt(canvas.style.height.replace('px', ''))) || canvas.height,
@@ -111,8 +109,6 @@ var MobileControlsComponent = IgeEntity.extend({
             .height(540)
             .mount(this.mobileControls);
 
-        // this.guide._resizeEvent = this.handleResize.bind(this);
-
         // for testing layout / scaling is working OK
         if (this.debug) this.guide.texture(self.textures.guide);
 
@@ -121,181 +117,8 @@ var MobileControlsComponent = IgeEntity.extend({
             this.addButton('2', 960 - 64, 540 - 64, 64, 64);
         }
 
-        // attach event handlers
-        ige.input.on('mouseDown', function (event) {
-            return self._inputDown.call(self, event);
-        });
-        ige.input.on('mouseMove', function (event) {
-            return self._inputMove.call(self, event);
-        });
-        ige.input.on('mouseUp', function (event) {
-            return self._inputUp.call(self, event);
-        });
-
         return this;
 
-    },
-
-    // canvas point from mouse event
-    mousePoint: function (e) {
-        return new IgePoint2d(
-            e.igeX - ige._bounds2d.x2,
-            e.igeY - ige._bounds2d.y2
-        );
-    },
-
-    // canvas point from touch event
-    touchPoint: function (touch) {
-        return new IgePoint2d(
-            touch.pageX - ige._canvasPosition().left - ige._bounds2d.x2,
-            touch.pageY - ige._canvasPosition().top - ige._bounds2d.y2
-        );
-    },
-
-    // convert canvas point to viewport point
-    canvasToViewportPoint: function (canvasPoint) {
-        var vp = ige.client.vp1;
-        var viewportPoint = new IgePoint2d(
-            Math.floor((canvasPoint.x - vp._translate.x) / vp.camera._scale.x + vp.camera._translate.x),
-            Math.floor((canvasPoint.y - vp._translate.y) / vp.camera._scale.y + vp.camera._translate.y)
-        );
-        return viewportPoint;
-    },
-
-    // handle input down events
-    _inputDown: function (e) {
-        if (e.type == "mousedown") {
-            var mp = this.canvasToViewportPoint(this.mousePoint(e));
-            this.handleInputDown(mp, null);
-            return;
-        }
-
-        if (!e.changedTouches) {
-            return;
-        }
-
-        for (var i = 0; i < e.changedTouches.length; i++) {
-            var touch = e.changedTouches[i];
-            this.touchesDown[touch.identifier] = true;
-            var vpBounds = ige.pixi.viewport.getVisibleBounds();
-            var tp = { x: vpBounds.x + e.igeX, y: vpBounds.y + e.igeY };
-            this.handleInputDown(tp, touch);
-
-        }
-
-    },
-
-    // harmonized mouse / touch input handler
-    // touch object will be null if mouse
-    handleInputDown: function (pos, touch) {
-        var self = this;
-        //console.log('inputdown '+pos.x+' '+pos.y);
-        var isButtonPressed = false;
-        for (ix in this.controls) {
-            var button = self.controls[ix];
-            if (typeof button === 'object' && button && this.isButtonPressed()) {
-                isButtonPressed = true;
-                button && button.handleInputDown(pos, touch);
-            }
-        }
-        if (!isButtonPressed) {
-            for (ix in this.controls) {
-                var button = self.controls[ix];
-                if (button && typeof button === 'object') {
-                    button.handleInputDown(pos, touch);
-                }
-            }
-        }
-    },
-
-    // harmonized mouse / touch input handler
-    // touch object will be null if mouse
-    handleInputMove: function (pos, touch) {
-        var self = this;
-        //console.log('inputmove '+pos.x+' '+pos.y);
-        for (ix in this.controls) {
-            var button = self.controls[ix];
-            if (typeof button === 'object' && button) {
-                button.handleInputMove(pos, touch);
-            }
-        }
-    },
-    isWheel: function (ix) {
-        return ix == 'movementWheel' || ix == 'lookWheel' || ix == 'lookAndFireWheel';
-    },
-    isMovementOrLookWheelEnable: function () {
-        for (ix in this.controls) {
-            if (this.isWheel(ix) && this.controls[ix]._touchId) {
-                return true;
-            }
-        }
-        return false;
-    },
-
-    // harmonized mouse / touch input handler
-    // touch object will be null if mouse
-    handleInputUp: function (pos, touch) {
-        var self = this;
-        //console.log('inputup '+pos.x+' '+pos.y);
-        for (ix in this.controls) {
-            var button = self.controls[ix];
-            if (typeof button === 'object' && button && (!this.isMovementOrLookWheelEnable() || this.isWheel(ix))) {
-                button.handleInputUp(pos, touch);
-            }
-        }
-    },
-
-
-
-    // handle input move events
-    _inputMove: function (e) {
-        if (e.type == "mousemove") {
-            var mp = this.canvasToViewportPoint(this.mousePoint(e));
-            this.handleInputMove(mp, null);
-            return;
-        }
-
-        if (!e.changedTouches) {
-            return;
-        }
-        e.preventDefault();
-        for (var i = 0; i < e.changedTouches.length; i++) {
-            var touch = e.changedTouches[i];
-            //console.log('move '+touch.identifier);
-            if (this.touchesDown[touch.identifier] == true) {
-                var tp = this.canvasToViewportPoint(this.touchPoint(touch));
-                this.handleInputMove(tp, touch);
-            }
-        }
-
-    },
-
-    // handle input up events
-    _inputUp: function (e) {
-        if (e.type == "mouseup") {
-            var mp = this.canvasToViewportPoint(this.mousePoint(e));
-            this.handleInputUp(mp, null);
-            return;
-        }
-        if (!e.changedTouches) {
-            return;
-        }
-        for (var i = 0; i < e.changedTouches.length; i++) {
-            var touch = e.changedTouches[i];
-            this.touchesDown[touch.identifier] = false;
-            //console.log('up '+touch.identifier);
-            var tp = this.canvasToViewportPoint(this.touchPoint(touch));
-            this.handleInputUp(tp, touch);
-        }
-    },
-    isButtonPressed: function () {
-        for (var key in this.controls) {
-            var control = this.controls[key];
-            if (typeof button === 'object' && control.isClicked) {
-                return true;
-            }
-        }
-        return false;
     },
 
     clearControls: function (newAbilities) {
@@ -319,8 +142,6 @@ var MobileControlsComponent = IgeEntity.extend({
 
         if (!this.isMobile || !abilities) return;
 		
-		
-
         // $("#show-chat").show();
         $("#show-chat").hide(); // completely disable chat on mobile (app review)
         //$("#chat-box").show();
@@ -440,49 +261,6 @@ var MobileControlsComponent = IgeEntity.extend({
         // return ige._bounds2d.x < ige._bounds2d.y;
         // return window.orientation === 0 || window.orientation == undefined;
         return this.canvas.width < this.canvas.height
-    },
-
-    // on canvas resize scale guide entity to fit canvas bounds
-    handleResize: function (event) {
-        var scaleFitX = ige._bounds2d.x / 960;
-        var scaleFitY = ige._bounds2d.y / 540;
-
-        var guideScale = scaleFitX;
-        if (scaleFitY < scaleFitX) {
-            guideScale = scaleFitY;
-        }
-
-        if (this.guide) {
-            this.guide.scaleTo(guideScale, guideScale, guideScale);
-        }
-    },
-
-    _behaviour: function () {
-        for (var key in this.controls) {
-            var button = this.controls[key];
-            if (typeof button !== 'object') return;
-            if (button.isJoyStick) {
-                var basePos = button.getPositionWrtPixiVP(button.base._pixiContainer.mousePosDown);
-                var stickPos = button.getPositionWrtPixiVP(button.stick.mouseDown ?
-                    button.stick._pixiContainer.lastMousePos :
-                    button.stick._pixiContainer.mousePosDown,
-                    true
-                );
-                if (basePos) {
-                    button.base._pixiContainer.position.set(basePos.x, basePos.y);
-                }
-                if (stickPos) {
-                    button.stick._pixiContainer.position.set(stickPos.x, stickPos.y);
-                }
-            }
-            else {
-                var originalPoints = button.originalPoints;
-                if (originalPoints) {
-                    var worldPoint = ige.pixi.viewport.toWorld(originalPoints);
-                    button._pixiContainer.position.set(worldPoint.x, worldPoint.y);
-                }
-            }
-        }
     }
 
 });
