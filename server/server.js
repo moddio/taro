665173ca388092e7f093f529a7b51926130b9063
antocorr <navylon@gmail.com>
2021-02-10 12:20:47 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -278,7 +278,7 @@ var Server = IgeClass.extend({
 		app.use('/assets', express.static(path.resolve('./assets/'), { cacheControl: 7 * 24 * 60 * 60 * 1000 }));
 
 		app.get('/', (req, res) => {
-			const videoChatEnabled = ige.game.videoChatEnabled ? ige.game.videoChatEnabled : false;
+			const videoChatEnabled = ige.game.videoChatEnabled && req.protocol == "https" ? ige.game.videoChatEnabled : false;
 			//const videoChatEnabled = true;
 			const game = {
 				_id: global.standaloneGame.defaultData._id,
