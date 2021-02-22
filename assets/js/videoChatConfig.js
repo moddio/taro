8219cc9997b82432078dfd1f2fb687f4651491f8
parth vakharia <pvakharia007@gmail.com>
2021-02-22 21:27:54 +0530

    creds restored

--- assets/js/videoChatConfig.js
+++ assets/js/videoChatConfig.js
@@ -3,15 +3,16 @@ const videoChatConfig = {
         config: {
             'iceServers': [
                 { url: 'stun:stun.l.google.com:19302' },
-                { url: 'turn:209.145.53.146', username: 'user', credential: 'pass' }
+
             ]
         } /* Sample servers, please use appropriate ones */,
-        host: 'videochat.resetstudio.it',
+        host: '',
         secure: true,
         path: 'peerjs/myapp',
         port: '3000'
     },
     socketIOConfig: {
-        url: 'https://videochat.resetstudio.it:3000/'
+        url: ''
     }
+
 }
