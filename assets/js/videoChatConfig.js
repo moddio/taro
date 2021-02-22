e9d2c396382dd7d3836311f35ff45cfa5319f30a
parth vakharia <pvakharia007@gmail.com>
2021-02-22 21:26:31 +0530

    fix: video chat config

--- assets/js/videoChatConfig.js
+++ assets/js/videoChatConfig.js
@@ -3,16 +3,15 @@ const videoChatConfig = {
         config: {
             'iceServers': [
                 { url: 'stun:stun.l.google.com:19302' },
-
+                { url: 'turn:209.145.53.146', username: 'user', credential: 'pass' }
             ]
         } /* Sample servers, please use appropriate ones */,
-        host: '',
+        host: 'videochat.resetstudio.it',
         secure: true,
         path: 'peerjs/myapp',
         port: '3000'
     },
     socketIOConfig: {
-        url: ''
+        url: 'https://videochat.resetstudio.it:3000/'
     }
-
 }
