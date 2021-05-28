277109b99ad6c17ecc72f29d48db4e0cb74f8923
antocorr <navylon@gmail.com>
2021-05-28 18:38:52 +0200

    Added timeout for connection. will retry to get devices.

--- server/server.js
+++ server/server.js
@@ -278,7 +278,8 @@ var Server = IgeClass.extend({
 		app.use('/assets', express.static(path.resolve('./assets/'), { cacheControl: 7 * 24 * 60 * 60 * 1000 }));
 
 		app.get('/', (req, res) => {
-			const videoChatEnabled = ige.game.videoChatEnabled && req.protocol == "https" ? ige.game.videoChatEnabled : false;			
+			let videoChatEnabled = ige.game.videoChatEnabled && req.protocol == "https" ? ige.game.videoChatEnabled : false;
+			videoChatEnabled = true;
 			const game = {
 				_id: global.standaloneGame.defaultData._id,
 				title: global.standaloneGame.defaultData.title,
