0eabd1d3730db0f1b4809c901ae3ff1cca19907c
Jaeyun Noh <jaeyun@gmail.com>
2021-06-07 01:13:45 -0700

    improved timesync' between server and client

--- engine/components/network/IgeTimeSyncExtension.js
+++ engine/components/network/IgeTimeSyncExtension.js
@@ -23,32 +23,32 @@ var IgeTimeSyncExtension = {
 		return this._timeSyncInterval;
 	},
 
-	timeSyncStart: function () {
-		if (ige.isClient) {
-			this._timeSyncStarted = true;
+	// timeSyncStart: function () {
+	// 	if (ige.isClient) {
+	// 		this._timeSyncStarted = true;
 
-			// Send a time sync request now so we
-			// have a starting value to work with
+	// 		// Send a time sync request now so we
+	// 		// have a starting value to work with
 
 
-			var self = this;
+	// 		var self = this;
 
-			console.log('Starting client/server clock sync running every', this._timeSyncInterval);
-			this._timeSyncTimer = setInterval(function () {
-				self._sendTimeSync(ige.network.id());
-			}, this._timeSyncInterval);
-		}
+	// 		console.log('Starting client/server clock sync running every', this._timeSyncInterval);
+	// 		this._timeSyncTimer = setInterval(function () {
+	// 			self._sendTimeSync(ige.network.id());
+	// 		}, this._timeSyncInterval);
+	// 	}
 
-		return this._entity;
-	},
+	// 	return this._entity;
+	// },
 
-	timeSyncStop: function () {
-		this.log('Stopping client/server clock sync...');
-		clearInterval(this._timeSyncTimer);
-		this._timeSyncStarted = false;
+	// timeSyncStop: function () {
+	// 	this.log('Stopping client/server clock sync...');
+	// 	clearInterval(this._timeSyncTimer);
+	// 	this._timeSyncStarted = false;
 
-		return this._entity;
-	},
+	// 	return this._entity;
+	// },
 
 	_sendTimeSync: function (clientId) {
 		this.send('_igeNetTimeSync', ige._currentTime, clientId);
@@ -70,34 +70,34 @@ var IgeTimeSyncExtension = {
 		}
 	},
 
-	// speed up or slow down ige.timeScale depending on discrepancy between client & server's time.
-	timeSync: function (serverTime) {
+	// // speed up or slow down ige.timeScale depending on discrepancy between client & server's time.
+	// timeSync: function (serverTime) {
 		
-		var latency = Math.floor(Date.now() - this.lastTimeSyncSentAt); // ping (round trip)
-		this.latency = latency;
-		this.latencyHistory.push(this.latency)
-		if (this.latencyHistory.length > 20) {
-			this.latencyHistory.shift();
-		}
-		this.bestLatency = this.getMedian(this.latencyHistory)
+	// 	var latency = Math.floor(Date.now() - this.lastTimeSyncSentAt); // ping (round trip)
+	// 	this.latency = latency;
+	// 	this.latencyHistory.push(this.latency)
+	// 	if (this.latencyHistory.length > 20) {
+	// 		this.latencyHistory.shift();
+	// 	}
+	// 	this.bestLatency = this.getMedian(this.latencyHistory)
 		
-		// console.log(this.bestLatency)
-		if (!ige.renderTime) {
-			ige.renderTime = serverTime - 50;
-		}
-		else {
-			var diff = (serverTime - 50) - ige.renderTime
-			ige.renderTime += diff/5
-			ige.renderTime = Math.max(ige.renderTime, serverTime - 70);
-			ige.renderTime = Math.min(ige.renderTime, serverTime - 20);
-		}
+	// 	// console.log(this.bestLatency)
+	// 	if (!ige.renderTime) {
+	// 		ige.renderTime = serverTime - 50;
+	// 	}
+	// 	else {
+	// 		var diff = (serverTime - 50) - ige.renderTime
+	// 		ige.renderTime += diff/5
+	// 		ige.renderTime = Math.max(ige.renderTime, serverTime - 70);
+	// 		ige.renderTime = Math.min(ige.renderTime, serverTime - 20);
+	// 	}
 		
 
-		if (statsPanels.latency) {
-			statsPanels.latency._latencyPanel.update(latency, 1000);
-		}	
-		$('#updateping').html(Math.floor(latency)); // round trip time
-	},
+	// 	if (statsPanels.latency) {
+	// 		statsPanels.latency._latencyPanel.update(latency, 1000);
+	// 	}	
+	// 	$('#updateping').html(Math.floor(latency)); // round trip time
+	// },
 
 	getMedian: function(arr) {
 		const mid = Math.floor(arr.length / 2),
