3572e20c2036a4b93519800e530f9f25ac43637b
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2022-02-21 19:45:37 +0100

    typescript sources and declarations

---
+++ ts/src/index.ts
@@ -0,0 +1,23 @@
+var Game = IgeClass.extend({
+	classId: 'Game',
+
+	init (App: typeof Client, options?: object) {
+		// Create the engine
+		ige = new IgeEngine(options);
+
+		console.log('ige initialized', ige.isClient, ige.isServer);
+		if (ige.isClient) {
+			ige.client = new App();
+		}
+
+		if (ige.isServer) {
+			ige.server = new App(options);
+		}
+	}
+});
+
+if (typeof (module) !== 'undefined' && typeof (module.exports) !== 'undefined') {
+	module.exports = Game;
+} else {
+	var game = new Game(Client);
+}
