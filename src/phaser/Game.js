473beb38142028050d8b4aa3017d78d97a291d6f
Pavle Goloskokovic <pgoloskokovic@gmail.com>
2022-02-21 20:04:16 +0100

    generated phaser test classes with source maps

---
+++ src/phaser/Game.js
@@ -0,0 +1,30 @@
+var __extends = (this && this.__extends) || (function () {
+	var extendStatics = function (d, b) {
+		extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) {
+            	d.__proto__ = b;
+            }) ||
+            function (d, b) {
+            	for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p];
+            };
+		return extendStatics(d, b);
+	};
+	return function (d, b) {
+		if (typeof b !== 'function' && b !== null)
+			throw new TypeError(`Class extends value ${  String(b)  } is not a constructor or null`);
+		extendStatics(d, b);
+		function __() {
+			this.constructor = d;
+		}
+		d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+	};
+})();
+var PhaserGame = /** @class */ (function (_super) {
+	__extends(PhaserGame, _super);
+	function PhaserGame() {
+		return _super !== null && _super.apply(this, arguments) || this;
+	}
+	return PhaserGame;
+}(Phaser.Game));
+new PhaserGame();
+//# sourceMappingURL=Game.js.map
---
+++ src/phaser/Test.js
@@ -0,0 +1,31 @@
+var __extends = (this && this.__extends) || (function () {
+	var extendStatics = function (d, b) {
+		extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) {
+            	d.__proto__ = b;
+            }) ||
+            function (d, b) {
+            	for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p];
+            };
+		return extendStatics(d, b);
+	};
+	return function (d, b) {
+		if (typeof b !== 'function' && b !== null)
+			throw new TypeError(`Class extends value ${  String(b)  } is not a constructor or null`);
+		extendStatics(d, b);
+		function __() {
+			this.constructor = d;
+		}
+		d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+	};
+})();
+var Test = /** @class */ (function (_super) {
+	__extends(Test, _super);
+	function Test() {
+		var _this = _super.call(this) || this;
+		console.log('Test again');
+		return _this;
+	}
+	return Test;
+}(HTMLCanvasElement));
+//# sourceMappingURL=Test.js.map
