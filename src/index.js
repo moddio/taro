632ca7e2930ed54946ca611d9cf62766839948ef
Sloont <colinabel93@gmail.com>
2022-02-23 15:14:00 -0500

    left conflict inside index.js. fixed now

--- src/index.js
+++ src/index.js
@@ -3,12 +3,8 @@ var Game = IgeClass.extend({
 	init: function (App, options) {
 		// Create the engine
 		ige = new IgeEngine(options);
-<<<<<<< HEAD
 
 		console.log('ige initialized', 'Client: ', ige.isClient, '  Server: ', ige.isServer);
-=======
-		console.log('ige initialized', ige.isClient, ige.isServer);
->>>>>>> e7053f08f5f9fc1e08b26cee230323dbeaa21371
 		if (ige.isClient) {
 			ige.client = new App();
 		}
