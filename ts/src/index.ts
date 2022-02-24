9fec3baeefba4afc2ceb1b49570380210cc3b988
Damien Vesper <ldamienvesper@gmail.com>
2022-02-24 15:56:10 -0600

    switch ts lint back to quotes

--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,17 +1,17 @@
 const Game = IgeClass.extend({
-	classId: `Game`,
+	classId: 'Game',
 
 	init (App: typeof Client, options?: Record<string, unknown>) {
 		// Create the engine
 		ige = new IgeEngine(options);
-		console.log(`ige initialized`, ige.isClient, ige.isServer); // debug?
+		console.log('ige initialized', ige.isClient, ige.isServer); // debug?
 
 		if (ige.isClient) ige.client = new App();
 		if (ige.isServer) ige.server = new App(options);
 	}
 });
 
-if (typeof (module) !== `undefined` && typeof (module.exports) !== `undefined`) {
+if (typeof (module) !== 'undefined' && typeof (module.exports) !== 'undefined') {
 	module.exports = Game; // TBD: Use ESM exports instead.
 } else {
 	/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
