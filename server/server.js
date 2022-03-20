e72ee084cd1d27f4c920ec1fc7dfee39a1ee28aa
KL0 <85732105+KL0maboi@users.noreply.github.com>
2022-03-20 14:02:48 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -565,7 +565,8 @@ var Server = IgeClass.extend({
 		ige.network.define('playerCustomInput', self._onPlayerCustomInput);
 		ige.network.define('playerAbsoluteAngle', self._onPlayerAbsoluteAngle);
 		ige.network.define('playerDialogueSubmit', self._onPlayerDialogueSubmit);
-
+		ige.network.define('playerButtonClick', self._onPlayerButtonClick);
+		
 		ige.network.define('buyItem', self._onBuyItem);
 		ige.network.define('buyUnit', self._onBuyUnit);
 		ige.network.define('buySkin', self._onBuySkin);
