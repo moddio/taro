546dae630cad8e52063fc3aadfa34ea27ede297d
Grenagar <62058278+Grenagar@users.noreply.github.com>
2022-05-13 14:30:23 +0200

    remove pixi mouse input logic

--- ts/src/renderer/phaser/scenes/GameScene.ts
+++ ts/src/renderer/phaser/scenes/GameScene.ts
@@ -11,7 +11,6 @@ class GameScene extends Phaser.Scene {
 		canvas.style.position = 'fixed';
 		canvas.style.opacity = '0.5';
 		canvas.style.backgroundColor = 'transparent';
-		//canvas.style.pointerEvents = 'none'; // TODO remove after pixi is gone
 
 		const camera = this.cameras.main;
 
