156e37370c9719bef7243dd59d832546d497ced9
Sloont <colinabel93@gmail.com>
2022-05-27 13:44:59 -0400

    npm run tsc && npm run build

--- src/renderer/phaser/classes/PhaserRegion.js
+++ src/renderer/phaser/classes/PhaserRegion.js
@@ -34,14 +34,15 @@ var PhaserRegion = /** @class */ (function (_super) {
         scene.add.existing(_this);
         _this.updateDimensionsListener = region.on('update-region-dimensions', function () {
             var stats = _this.region._stats.default;
+            // I didn't want to go too deep on the entity stream/update process, but because of the current logic,
+            // if we stream changes to (3) variables, this will fire (3) times.
             console.log("PhaserRegion update ".concat(region._stats.id, " ").concat(region._id)); // TODO: Remove
-            console.log(_this.x === stats.x, _this.y === stats.y, _this.width === stats.width, _this.height === stats.height); // TODO: Remove
             _this.x = stats.x;
             _this.y = stats.y;
             _this.width = stats.width;
             _this.height = stats.height;
             _this.clear();
-            _this.fillStyle(Number("0x".concat(stats.inside.substring(1))), 0.4 || stats.alpha / 100);
+            _this.fillStyle(Number("0x".concat(stats.inside.substring(1))), stats.alpha / 100 || 0.4);
             _this.fillRect(0, 0, stats.width, stats.height);
         });
         scene.events.on('update', _this.update, _this);
